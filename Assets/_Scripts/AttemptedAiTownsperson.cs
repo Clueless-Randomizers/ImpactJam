ad5740907ea1bbee75947a35f3b176b6ee5c15b8
masterITS <ingesela@gmail.com>
2024-04-13 17:43:59 +0200

    Made a bare bones ai move random script

---
+++ Assets/_Scripts/AttemptedAiTownsperson.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AttemptedAiTownsperson : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    
+    private bool isMoving = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(PickRandomLocation), 0, 5);
+    }
+
+    private void PickRandomLocation()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * 20;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, 20, 1);
+        Vector3 finalPosition = hit.position;
+        agent.SetDestination(finalPosition);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
