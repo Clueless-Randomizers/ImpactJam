387fea8a05197d2f12af1f0e6b208992906de1fd
Tor-Arne Sandstrak <github@logi.pm>
2024-04-13 17:19:40 +0200

    GatheringAI now walks around to gather their stuff.

--- Assets/_Scripts/Characters/GatheringAI.cs
+++ Assets/_Scripts/Characters/GatheringAI.cs
@@ -1,21 +1,36 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent( typeof( NavMeshAgent ) )]
 public class GatheringAI : MonoBehaviour
 {
 	[SerializeField] GatheringBuilding _gatheringBuilding = default;
+	[SerializeField] private int _maxFoundResources = 5;
+
 	float _nextGatheringRun = 0f;
 	float _gatheringTimeout = 20f;
 	float _timeSpentGathering = 30f;
-	bool _shouldGather = false;
+	float _nextReturnTime = 0f;
+
 	bool _isGathering = false;
+	bool _hasResources = false;
+	
+	int _carryingResources = 0;
 
+	NavMeshAgent _agent;
 
-    void Update()
+	private void Start () {
+		_agent = transform.GetComponent<NavMeshAgent>();
+		_agent.isStopped = false;
+
+		if ( _gatheringBuilding != default) {
+			SetGatheringBuilding( _gatheringBuilding );
+		}
+	}
+
+	void Update()
     {
-        if ( _shouldGather && !_isGathering && _nextGatheringRun < Time.realtimeSinceStartup) {
+        if ( !_hasResources && !_isGathering && _nextGatheringRun < Time.realtimeSinceStartup) {
 			_nextGatheringRun = Time.realtimeSinceStartup + _gatheringTimeout;
 			_isGathering = true;
 			StartGathering();
@@ -24,19 +39,47 @@ public class GatheringAI : MonoBehaviour
     }
 
 	private void CheckIfDoneGathering () {
-		
+		if ( _isGathering && _nextReturnTime < Time.realtimeSinceStartup) {
+			GoToDestination(_gatheringBuilding.transform.position);
+			_hasResources = true;
+			_isGathering = false;
+			_carryingResources = ( int ) 1 * Random.Range( 1 , _maxFoundResources );
+		}
+	}
+
+	private void GoToDestination ( Vector3 position ) {
+		_agent.SetDestination(position);
+		_agent.isStopped = false;
 	}
 
 	private void StartGathering () {
 		// This is where I would have loved to have a "Forest Zone" and be able to tell my person to walk to a random place inside that zone. 
 		// We'll just do random location.
+		Vector3 _targetPosition = new Vector3(Random.Range( 0f, 885f),0f, Random.Range( 0f, 885f ) );
 
+		_isGathering = true;
+		_nextReturnTime = Time.realtimeSinceStartup + _timeSpentGathering;
+
+		GoToDestination( _targetPosition );
 	}
 
 	public void SetGatheringBuilding(GatheringBuilding gatheringBuilding) {
-		_gatheringBuilding = gatheringBuilding;
-		_gatheringTimeout = _gatheringBuilding.GatheringTime;
-		_timeSpentGathering = gatheringBuilding.TimeToGather;
+		if ( gatheringBuilding.AddGatheringPerson( this ) ) {
+			_gatheringBuilding = gatheringBuilding;
+
+			_gatheringTimeout = _gatheringBuilding.GatheringTime;
+			_timeSpentGathering = gatheringBuilding.TimeToGather;
+		}
+	}
+
+	private void OnTriggerEnter ( Collider other ) {
+		if ( _hasResources && other.transform.TryGetComponent(out GatheringBuilding gatheringBuilding)) {
+			gatheringBuilding.AddCurrency(_carryingResources);
+			_carryingResources = 0;
+			_agent.SetDestination( default );
+			_agent.isStopped = true;
+			_hasResources = false;
+		}
 	}
 }
 /*
