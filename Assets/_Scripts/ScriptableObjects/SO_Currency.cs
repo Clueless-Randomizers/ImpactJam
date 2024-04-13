1d8c2b03969aa1ba5b38c79b13c6df695c9102d8
Tor-Arne Sandstrak <jobb@tor-arne.com>
2024-04-13 07:14:07 +0200

    Moved CurrencyScriptableObject, renamed to SO_Currency, added CurrencyChange event.

---
+++ Assets/_Scripts/ScriptableObjects/SO_Currency.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// This ScriptableObject is the base for any currency in this game.
+/// </summary>
+[Serializable]
+[CreateAssetMenu( menuName = "ScriptableAssets/New Currency" )]
+public class SO_Currency : ScriptableObject
+{
+	[SerializeField] int _currentCurrency = 0;
+	[SerializeField] Sprite _currencyImage;
+	[SerializeField] CurrencyType _currencyType;
+	public Sprite Icon { get { return _currencyImage; } }
+	public UnityEvent CurrencyChange;
+	public int Value { 
+		get { return _currentCurrency; } 
+		set { 
+			int _newValue = _currentCurrency + value;  
+			if (_currentCurrency != _newValue) {
+				CurrencyChange?.Invoke();
+			} 
+			_currentCurrency = ( _newValue > 0) ? _newValue : 0; 
+		} 
+	}
+
+}
+[Serializable]
+public enum CurrencyType {
+	Food,
+	Wood,
+	Heritage
+}
