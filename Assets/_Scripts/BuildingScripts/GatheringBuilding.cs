ee1cb530f6fb3ab0fcbf4b96df11141c906b7a27
Tor-Arne Sandstrak <github@logi.pm>
2024-04-14 13:09:41 +0200

    Commented functionality in GatheringBuilding, renamed GatheringTime to WrokerRestTime.

--- Assets/_Scripts/BuildingScripts/GatheringBuilding.cs
+++ Assets/_Scripts/BuildingScripts/GatheringBuilding.cs
@@ -13,12 +13,26 @@ public class GatheringBuilding : MonoBehaviour
 		_buildingCurrency = GameManager.CurrencyManager.GetCurrency( _buildingCurrency.name );
 	}
 
-	public float GatheringTime { get { return _timeBetweenGathering; } }
+	/// <summary>
+	/// Get this building's time between gathering.
+	/// </summary>
+	public float WorkerRestTime { get { return _timeBetweenGathering; } }
 
+	/// <summary>
+	/// Get this building's time spent gathering.
+	/// </summary>
 	public float TimeSpentGathering { get { return _timeSpentGathering; } }
 
+	/// <summary>
+	/// Get this building's currency.
+	/// </summary>
 	public SO_Currency GetCurrency {  get { return _buildingCurrency; } }
 
+	/// <summary>
+	/// Add person to gathering position in building provided there is enough room.
+	/// </summary>
+	/// <param name="gatheringAI"></param>
+	/// <returns></returns>
 	public bool AddGatheringPerson ( GatheringAI gatheringAI ) {
 		if ( _gatheringPeople.Count < _maxGatheringPeople && !_gatheringPeople.Contains( gatheringAI ) ) {
 			_gatheringPeople.Add( gatheringAI );
@@ -27,8 +41,28 @@ public class GatheringBuilding : MonoBehaviour
 			return false;
 		};
 	}
-
+	/// <summary>
+	/// Remove person from gathering position in building.
+	/// </summary>
+	/// <param name="gatheringAI"></param>
+	public void RemoveGatheringPerson( GatheringAI gatheringAI ) {
+		if ( _gatheringPeople.Contains( gatheringAI ) ) {
+			_gatheringPeople.Remove( gatheringAI );
+		}
+	}
+	/// <summary>
+	/// Add to the currency being gathered.
+	/// </summary>
+	/// <param name="currency"></param>
 	public void AddCurrency (int currency) {
 		_buildingCurrency.Value = currency;
+	}
+
+	/// <summary>
+	/// Get Worker's hat-material.
+	/// </summary>
+	/// <returns></returns>
+	public Material GetHatMaterial() { 
+		return _gathererHatMaterial;
 	}
 }
