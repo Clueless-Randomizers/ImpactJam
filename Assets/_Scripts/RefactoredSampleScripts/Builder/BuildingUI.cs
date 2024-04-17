cbb9f51f78b446d74424ee6ca0f53724408e16bb
Tor-Arne Sandstrak <github@logi.pm>
2024-04-17 11:57:48 +0200

    Matching tabification

--- Assets/_Scripts/RefactoredSampleScripts/Builder/BuildingUI.cs
+++ Assets/_Scripts/RefactoredSampleScripts/Builder/BuildingUI.cs
@@ -6,54 +6,54 @@ using UnityEngine.UI;
 
 namespace _Scripts.RefactoredSampleScripts.Builder
 {
-    public class BuildingUI : MonoBehaviour
-    {
-        CanvasGroup canvasGroup;
-        bool isPlacing = false;
-        int currentIndex = 0;
-        
-        Mesh buildingPreviewMesh;
-        [SerializeField] Material buildingPreviewMat;
-        private void Awake()
-        {
-            canvasGroup = GetComponent<CanvasGroup>();
-        }
-        void Start()
-        {
-            Button[] buttons = GetComponentsInChildren<Button>();
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                int index = i;
-                buttons[index].onClick.AddListener(() => SelectBuilding(index));
+	public class BuildingUI : MonoBehaviour
+	{
+		CanvasGroup canvasGroup;
+		bool isPlacing = false;
+		int currentIndex = 0;
+		
+		Mesh buildingPreviewMesh;
+		[SerializeField] Material buildingPreviewMat;
+		private void Awake()
+		{
+			canvasGroup = GetComponent<CanvasGroup>();
+		}
+		void Start()
+		{
+			Button[] buttons = GetComponentsInChildren<Button>();
+			for (int i = 0; i < buttons.Length; i++)
+			{
+				int index = i;
+				buttons[index].onClick.AddListener(() => SelectBuilding(index));
 
-                Building b = BuildingManager.instance.buildingPrefabs[index];
-                buttons[index].GetComponentInChildren<TextMeshProUGUI>().text = GetButtonText(b);
-            }
-        }
+				Building b = BuildingManager.instance.buildingPrefabs[index];
+				buttons[index].GetComponentInChildren<TextMeshProUGUI>().text = GetButtonText(b);
+			}
+		}
 
-        private void Update()
-        {
-            if (isPlacing)
-            {
-                Vector3 position = Utility.MouseToTerrainPosition();
-                Graphics.DrawMesh(buildingPreviewMesh, position, Quaternion.identity, buildingPreviewMat, 0);
-                if (Input.GetMouseButtonDown(0))
-                {
-                    BuildingManager.instance.SpawnBuilding(currentIndex, position);
-                    canvasGroup.alpha = 1;
-                    isPlacing = false;
-                }
-            }
-        }
+		private void Update()
+		{
+			if (isPlacing)
+			{
+				Vector3 position = Utility.MouseToTerrainPosition();
+				Graphics.DrawMesh(buildingPreviewMesh, position, Quaternion.identity, buildingPreviewMat, 0);
+				if (Input.GetMouseButtonDown(0))
+				{
+					BuildingManager.instance.SpawnBuilding(currentIndex, position);
+					canvasGroup.alpha = 1;
+					isPlacing = false;
+				}
+			}
+		}
 
-        void SelectBuilding(int index)
-        {
-            currentIndex = index;
-            ActorManager.instance.DeselectActors();
-            canvasGroup.alpha = 0;
-            isPlacing = true;
-            buildingPreviewMesh = BuildingManager.instance.GetPrefab(index).GetComponentInChildren<MeshFilter>().sharedMesh;
-        }
+		void SelectBuilding(int index)
+		{
+			currentIndex = index;
+			ActorManager.instance.DeselectActors();
+			canvasGroup.alpha = 0;
+			isPlacing = true;
+			buildingPreviewMesh = BuildingManager.instance.GetPrefab(index).GetComponentInChildren<MeshFilter>().sharedMesh;
+		}
 
 		string GetButtonText(Building b)
 		{
